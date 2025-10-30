202af00893a5cead01bf125d373fdc2f42b22dfa
romhein-games <119111870+romhein-games@users.noreply.github.com>
2025-10-30 17:03:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// === Effet Parallax ===
-const bg = document.querySelector('main');
-window.addEventListener('scroll', () => {
-  const vitesse = 0.4; // Ajuste la vitesse du défilement
-  const offset = window.scrollY * vitesse;
-  bg.style.transform = `translateY(${offset}px)`;
-});
 
 // Scroll fluide
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
