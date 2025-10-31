b8a539fb8f6e109c25c9c8dc02c0031f8ea83ab5
romhein-games <119111870+romhein-games@users.noreply.github.com>
2025-10-31 18:53:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // Facultatif : supprime le focus après le clic (utile sur mobile)
     card.addEventListener('touchend', () => card.blur());
   });
+
+  // Gestion des about
+  const aboutSections = document.querySelectorAll('.about');
+  aboutSections.forEach(section => {
+    section.addEventListener('click', () => {
+      const link = section.dataset.link;
+      if(link) {
+        window.location.href = link;
+      }
+    });
+    section.addEventListener('touchend', () => section.blur());
+  });
 });
